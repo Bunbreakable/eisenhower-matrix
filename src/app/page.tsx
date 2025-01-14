087108955363d87b64614d76686ce12106512801
Nico Alvarez M1 <nicoalvareza22@gmail.com>
2025-01-14 23:00:51 +0100

    chore: change home page button color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       {/* Plan Your Tasks Button */}
       <div className="flex items-center justify-center">
         <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-purple-500 text-background gap-2 hover:bg-purple-600 dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
           href="/planner"
         >
           Start Planning
